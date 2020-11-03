a84afc55ac5cb4302af5fa050a39901cd92d7bc7
limjen427 <limjen427@gmail.com>
2020-11-02 22:06:28 -0600

    Extends Summary in Course to use 5 getter in summary in Course.java and also add getDescription in Course.java.

---
+++ app/src/main/java/edu/illinois/cs/cs125/fall2020/mp/models/Course.java
@@ -0,0 +1,15 @@
+package edu.illinois.cs.cs125.fall2020.mp.models;
+
+/**
+ * Course class.
+ */
+public class Course extends Summary {
+    /**
+     * take the course discription in this getter.
+     * @return String
+     */
+    public String getDescription() {
+        assert false : "didn't out anything in getDescription method, ADD";
+        return "";
+    }
+}
